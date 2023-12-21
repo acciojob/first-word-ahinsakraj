e3dc80d8c076d902bc7c4dd3e82563a16f0ab2cd
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 16:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	const str=s.trim();
+	let ans='';
+	for(let i=0;i<str.length;i++){
+		if(str[i]==' ')return ans;
+		ans+=str[i];
+	}
+	return ans;
 }
 
 // Do not change the code below
